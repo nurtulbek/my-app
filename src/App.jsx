0593739797a9705efc821615d3eefb901bda80ba
nurtulbek <owezresul0@gmail.com>
2025-10-20 15:44:57 +0500

    assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import "./App.css"
 import me from "./assets/me.png"
+import hackhathon from "./assets/hackhathon.svg"
+import pasabahce from "./assets/pasabahce.svg"
 function App() {
 
   return (
@@ -16,14 +18,30 @@ function App() {
               <div className="platform">
               </div> 
             </div>
-
-            <div className="about">
-              <p>
-                Frontend Developer &
-                UI/UX Enthusiast
-              </p>
-            </div>
           </div>
+          <div className="about">
+            <p className="about-title">
+              Frontend Developer &
+               UI/UX Enthusiast
+            </p>
+
+            <p className="about-text">
+              I build responsive, fast, and modern websites with clean design. 
+              Skilled in React, Figma, and FlutterFlow with  backend knowledge 
+              to deliver complete solutions.
+            </p>
+
+            <button className="about-button">Let's work otgether</button>
+          </div>
+        </div>
+
+        <div className="works">
+            <div className="project-item">
+              <img src={hackhathon} alt="" />
+            </div>
+            <div className="project-item">
+              <img src={pasabahce} alt="" />
+            </div>
         </div>
       </div>
     </>
